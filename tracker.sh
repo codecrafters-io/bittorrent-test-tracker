a486c3660c9cf907a2ce5947d8a838cd8f0f01dc
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-22 18:06:43 +0100

    Update dummy username in chihaya.yaml

--- tracker.sh
+++ tracker.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i "s|REDIS_URL|${REDIS_URL}|" /etc/chihaya.yaml
-sed -i "s|redis://:|redis://x:|" /etc/chihaya.yaml # Add dummy username
+sed -i "s|redis://:|redis://default:|" /etc/chihaya.yaml # Add dummy username
 sed -i "s|0.0.0.0:8080|0.0.0.0:${PORT}|" /etc/chihaya.yaml
 echo ""
 cat /etc/chihaya.yaml
