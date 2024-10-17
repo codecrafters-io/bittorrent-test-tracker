46411cda4e71972b888399f33513b5a5bf10fcb9
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-10-17 18:28:19 +0100

    Update REDIS_URL to use REDIS_TEMPORARY_URL in chihaya.yaml configuration.

--- tracker.sh
+++ tracker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sed -i "s|REDIS_URL|${REDIS_URL}|" /etc/chihaya.yaml
+sed -i "s|REDIS_URL|${REDIS_TEMPORARY_URL}|" /etc/chihaya.yaml
 sed -i "s|redis://:|redis://|" /etc/chihaya.yaml # Add dummy username
 sed -i "s|0.0.0.0:8080|0.0.0.0:${PORT}|" /etc/chihaya.yaml
 echo ""
