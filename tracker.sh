aece8a13dd3e9d9f704810bef2308f288fa531f6
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-12-09 21:47:59 +0530

    Update tracker.sh to handle rediss URL for secure Redis connections.

--- tracker.sh
+++ tracker.sh
@@ -2,7 +2,8 @@
 set -e
 
 sed -i "s|REDIS_URL|${REDIS_URL}|" /etc/chihaya.yaml
-sed -i "s|redis://:|redis://|" /etc/chihaya.yaml # Add dummy username
+sed -i "s|redis://:|redis://|" /etc/chihaya.yaml   # Add dummy username
+sed -i "s|rediss://:|rediss://|" /etc/chihaya.yaml # Add dummy username (HTTPS)
 sed -i "s|0.0.0.0:8080|0.0.0.0:${PORT}|" /etc/chihaya.yaml
 echo ""
 cat /etc/chihaya.yaml
